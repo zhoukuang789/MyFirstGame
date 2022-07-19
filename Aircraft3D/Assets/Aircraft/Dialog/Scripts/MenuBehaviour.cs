5631d89efd006ff5d8bc4a6ecc1f320df5ae5763
Aieylisyh <Aieylisyh@users.noreply.github.com>
2022-07-19 10:34:50 +0800

    menu vignette

--- Aircraft3D/Assets/Aircraft/Dialog/Scripts/MenuBehaviour.cs
+++ Aircraft3D/Assets/Aircraft/Dialog/Scripts/MenuBehaviour.cs
@@ -4,38 +4,50 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Dialog.Scripts {
-    public class MenuBehaviour : MonoBehaviour {
+namespace Dialog.Scripts
+{
+    public class MenuBehaviour : MonoBehaviour
+    {
 
         public Transform buttons;
-        
+
+        public Image vignette;
         private int buttonCount;
 
-        private void Awake() {
+        private void Awake()
+        {
             buttonCount = 0;
         }
 
-        public MenuBehaviour SetText(string text) {
+        public MenuBehaviour SetText(string text)
+        {
             TextMeshProUGUI tmp = GetComponentInChildren<TextMeshProUGUI>();
             tmp.text = text;
             return this;
         }
-        
+
+        public void SetVignette(bool b)
+        {
+            vignette.enabled = b;
+        }
+
         /// <summary>
         /// 添加菜单按钮
         /// </summary>
-        public void AddButton(string text, Action onClick) {
+        public void AddButton(string text, Action onClick)
+        {
             buttonCount++;
             if (buttonCount > 3) return;
             Transform buttonTransform = buttons.transform.GetChild(buttonCount - 1);
             GameObject menuButtonPrefab = Resources.Load<GameObject>("Prefabs/MenuButton");
-            GameObject menuButton = Instantiate(menuButtonPrefab, buttonTransform.position, buttonTransform.rotation,buttons.transform);
+            GameObject menuButton = Instantiate(menuButtonPrefab, buttonTransform.position, buttonTransform.rotation, buttons.transform);
             ButtonBehaviour buttonBehaviour = menuButton.GetComponent<ButtonBehaviour>();
             buttonBehaviour.SetText(text).AddOnClick(onClick);
             menuButton.SetActive(true);
         }
 
-        public void Show() {
+        public void Show()
+        {
             gameObject.SetActive(true);
             CanvasGroup cg = GetComponent<CanvasGroup>();
             cg.DOFade(1, 2).SetDelay(0.25f);
