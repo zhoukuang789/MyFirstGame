215701ec3f641d98262e754c17127412c504e43f
zhoukuang789 <zhoukuang789@yahoo.com>
2022-08-02 11:42:34 +0800

    wip
    This reverts commit 0bf3be5fd4f6d0a59fb4cd4644e6c09be158891a.

--- Aircraft3D/Assets/Aircraft/MyVR/Scripts/VRBehaviour.cs
+++ Aircraft3D/Assets/Aircraft/MyVR/Scripts/VRBehaviour.cs
@@ -5,13 +5,15 @@ namespace MyVR {
     public class VRBehaviour : MonoBehaviour {
         public Transform cam;
 
-        // private SteamVR_GazeTracker tracker;
+        private SteamVR_TrackedObject trackedObj;
+        
 
         private void Start() {
-
+            trackedObj = GetComponent<SteamVR_TrackedObject>();
         }
 
         private void Update() {
+            
         }
     }
 }
