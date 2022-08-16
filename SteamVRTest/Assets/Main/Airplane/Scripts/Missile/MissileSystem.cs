c3e24bad815fc483a522b09f690c8c7cf3f1c2b6
Aieylisyh <Aieylisyh@users.noreply.github.com>
2022-08-16 11:53:35 +0800

    missile script

---
+++ SteamVRTest/Assets/Main/Airplane/Scripts/Missile/MissileSystem.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using com;
+
+public class MissileSystem : MonoBehaviour
+{
+    public float rechargeTime = 5;
+
+    float _rechargeTimer;
+    public Transform missile1Launcher;
+    public Transform missile2Launcher;
+    public Transform missileSpace;
+    public MissileMove missilePrefab;
+    public Transform target;
+
+    private void Start()
+    {
+        _rechargeTimer = 0;
+    }
+
+    void Update()
+    {
+        if (GetMissileInput())
+        {
+            TryLaunch();
+        }
+
+        if (_rechargeTimer > 0)
+        {
+            _rechargeTimer -= GameTime.deltaTime;
+        }
+    }
+
+    void TryLaunch()
+    {
+        if (_rechargeTimer > 0)
+        {
+            return;
+        }
+
+        Debug.Log("launch");
+        _rechargeTimer = rechargeTime;
+        var ms1 = Instantiate(missilePrefab, missile1Launcher.position, missile1Launcher.rotation, missileSpace);
+        var ms2 = Instantiate(missilePrefab, missile2Launcher.position, missile2Launcher.rotation, missileSpace);
+        ms1.Init(missile1Launcher.right, target);
+        ms2.Init(missile2Launcher.right, target);
+    }
+
+    bool GetMissileInput()
+    {
+        //TODO adapt VR
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ SteamVRTest/Assets/Main/Airplane/Scripts/Missile/MissileUi.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MissileUi : MonoBehaviour
+{
+    public void Sync()
+    {
+
+    }
+}
